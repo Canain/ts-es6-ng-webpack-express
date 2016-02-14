b7d71c001a4fc71d489e3144027d50d97671a626
Shuyang Chen <sc1085@gmail.com>
2016-02-14 08:07:56 -0500

    Fixed gitignore

--- server/js/app.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../../typings/main.d.ts" />
-'use strict';
-const port = 3000;
-const express = require('express');
-const app = express();
-app.use(express.static('pub')).listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
