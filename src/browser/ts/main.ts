3fb134696d2acd7f70cd3102db3b9f4b7d4f3a9e
Shuyang Chen <sc1085@gmail.com>
2016-05-21 17:43:37 +0800

    Added core-js

--- src/browser/ts/main.ts
+++ src/browser/ts/main.ts
@@ -1,6 +1,9 @@
-require("../css/style.css");
+'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
+require('../css/style.css');
+require('core-js');
+
+document.addEventListener('DOMContentLoaded', () => {
 	const h1 = document.createElement('h1');
 	h1.textContent = 'Hello';
 	document.body.appendChild(h1);
