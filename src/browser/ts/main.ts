a75760b2f0e6ee14dc28b7248fdbf3488386f42e
Shuyang Chen <sc1085@gmail.com>
2016-05-24 21:28:50 +0800

    Added angular 2

--- src/browser/ts/main.ts
+++ src/browser/ts/main.ts
@@ -1,10 +1,30 @@
 'use strict';
 
 require('../css/style.css');
+
 require('core-js');
 
-document.addEventListener('DOMContentLoaded', () => {
-	const h1 = document.createElement('h1');
-	h1.textContent = 'Hello';
-	document.body.appendChild(h1);
-});
+require('zone.js');
+require('reflect-metadata');
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { Component, enableProdMode } from '@angular/core';
+
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const h1 = document.createElement('h1');
+// 	h1.textContent = 'Hello';
+// 	document.body.appendChild(h1);
+// });
+
+@Component({
+	selector: 'webpack-express',
+	template: `
+		<h1>Hello</h1>
+	`
+})
+export class WebpackExpress {
+	
+}
+
+// enableProdMode();
+bootstrap(WebpackExpress);
